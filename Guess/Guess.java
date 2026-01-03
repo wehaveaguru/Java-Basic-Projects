70f65099364930beabd22fe33c61aaa7626f3189
Guru <207536326+wehaveaguru@users.noreply.github.com>
2026-01-03 12:21:10 +0530

    Changes made

--- Guess/Guess.java
+++ Guess/Guess.java
@@ -6,11 +6,11 @@ public class Guess{
         Scanner input=new Scanner(System.in);
         Random rnd=new Random();
         int ori=rnd.nextInt(50)+1;
-        System.out.println("Choose a number 1 till 50");
         for (int i=0;i<5;i++){
+            System.out.println("Choose a number 1 till 50");
             int num=input.nextInt();
             if (num!=ori){
-                System.out.println("Try again");
+                continue;
             }
         }
 
